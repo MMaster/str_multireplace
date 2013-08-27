07991f0a7b00a7cc272747cc5180ec84f960e058
Matej Spanik <mmaster@bitbix.com>
2013-08-28 01:24:42 +0200

    Implementation of Karp-Rabin algorithm modified for multiple pattern searching in source string. (with customizable hash macros)

--- src/str_multireplace.c
+++ src/str_multireplace.c
@@ -1,7 +1,8 @@
+
 /**
  * @file      str_multireplace.c
  * @brief     Multiple key-value replacement in provided string.
- * @author    Matej Spanik
+ * @author    Matej Spanik <mmaster@bitbix.com>
  * @version   0.1
  * @date      2013
  * @copyright Apache License v2
@@ -14,4 +15,201 @@
 
 #include "str_multireplace.h"
 
+/**
+ * @name String searching
+ *
+ * This section contains custom Karp-Rabin algorithm implementation optimized
+ * for multiple string searching at once.
+ */
 
+/** @{ */
+
+/**
+ * @brief Match pair internal wrapper structure
+ */
+typedef struct {
+    const str_mr_match_pair *pair;  /**< wrapped match pair */
+    uint64_t    key_hash;       /**< computed hash of the key */
+    uint64_t    str_hash;       /**< computed hash of substring of the same
+                                 * length as key */
+    uint64_t    rem_coef;       /**< character removal coeficient for
+                                 * UNHASH()/REHASH() */
+} str_mr_match_pair_wrap;
+
+/**
+ * @brief Compute hash character removal coefficient.
+ *
+ * Used for first hashed substring character removal in UNHASH() and REHASH()
+ *
+ * Computes (2^(match_len-1)). Only when match_len < 64 because hash has only
+ * 64 bits, anything beyond 64 bits is thrown away automatically.
+ *
+ * @param[in] match_len length of match string
+ * @return removal coefficient used in UNHASH() and REHASH()
+ */
+#define COMPUTE_REM_COEF(match_len) \
+        (match_len < 64?(1 << (match_len - 1)):0)
+
+/**
+ * @brief Hash new character into current hash.
+ *
+ * Used for getting hash of first substring (str[0..match_len]).
+ *
+ * @param[in] add_c character that comes to the hash (str[pos])
+ * @param[in] cur_hash hash of current matched substring at 
+ *            str[0..pos-1]
+ * @return hash of substring (str[0..pos])
+ */
+#define HASH(add_c, cur_hash) \
+        (((cur_hash) << 1) + (add_c))
+
+/**
+ * @brief Remove first character from hashed substring.
+ *
+ * Used for REHASH for quick computation of substring offsetted by 1.
+ *
+ * @param[in] rem_c first character from substring that goes away from hash
+ *            (str[pos])
+ * @param[in] cur_hash hash of current hashed substring
+ *            (str[pos..pos+match_len-1])
+ * @param[in] rem_coef preprocessed coefficient used in removal of first
+ *            character from hash (2^(match_len-1))
+ * @return hash of substring with first character removed 
+ *         (str[pos+1..pos+match_len-1])
+ */
+#define UNHASH(rem_c, cur_hash, rem_coef) \
+        ((cur_hash) - (rem_c)*(rem_coef))
+
+/**
+ * @brief Rehash counts hash of next substring of source string.
+ *
+ * Takes hash of current substring (str[pos..pos+match_len-1]) and counts hash
+ * of next substring offsetted by 1 (str[pos+1..pos+match_len]) by removing 
+ * first character of current substring and adding new character to the end of
+ * new substring by using HASH().
+ *
+ * @param[in] rem_c character that goes away from hash (str[pos])
+ * @param[in] add_c character that comes to the hash (str[pos+match_len])
+ * @param[in] cur_hash hash of current hashed substring 
+ *            (str[pos..pos+match_len-1])
+ * @param[in] rem_coef preprocessed coefficient used in removal of first 
+ *            character from hash (2^(match_len-1))
+ * @return hash of substring offsetted by 1 (str[pos+1..pos+match_len])
+ */
+#define REHASH(rem_c, add_c, cur_hash, rem_coef) \
+        HASH(add_c, UNHASH(rem_c, cur_hash, rem_coef))
+
+/**
+ * @brief String searching using Karp-Rabin algorithm 
+ *
+ * Searches for match in str. Doesn't care about NULL terminators.
+ *
+ * Note: there are no checks, but function has following assumptions:
+ * - str_len > match_len
+ * - str != NULL
+ * - match != NULL
+ *
+ * @param[in] str source string
+ * @param[in] str_len source string length
+ * @param[in] matches wrapped matches array SORTED by length (descending)
+ * @param[in] match_cnt count of matches
+ */
+static void
+str_mr_kr_search(const char *str, size_t str_len,
+                 const str_mr_match_pair_wrap *matches, size_t match_cnt)
+{
+    uint64_t    str_hash = 0;
+    uint64_t    match_hash = 0;
+    size_t      i = 1, j = 0, m = 0, first_valid_m = 0;
+    size_t      last_key_len = 0, last_str_hash = 0;
+    size_t      match_len = 0;
+    size_t      shortest_match_len = matches[match_cnt - 1].pair->key_length;
+    const char *match = NULL;
+
+    /* 
+     * count rem_coef for character removal from hash (UNHASH()/REHASH())
+     */
+    for (m = 0; m < match_cnt; m++) {
+        matches[m].rem_coef = COMPUTE_REM_COEF(matches[m].pair->key_length);
+    }
+
+    /* 
+     * count hash of first match_len characters of both match and str
+     * strings for each match 
+     */
+    for (m = 0; m < match_cnt; m++) {
+        matches[m].key_hash = matches[m].str_hash = 0;
+        match_len = matches[m].pair->key_length;
+
+        /* check if we didn't already count str_hash for this */
+        if (match_len == last_key_len) {
+            matches[m].str_hash = last_str_hash;
+
+            /* cool now we only need to copute key hash */
+            for (i = 0; i < match_len; i++) {
+                matches[m].key_hash = HASH(matches[m].pair->key[i],
+                                           matches[m].key_hash);
+            }
+            continue;
+        }
+
+        for (i = 0; i < match_len; i++) {
+            matches[m].key_hash =
+                    HASH(matches[m].pair->key[i], matches[m].key_hash);
+            matches[m].str_hash = HASH(str[i], matches[m].str_hash);
+        }
+
+        /* remember last match key length and str_hash */
+        last_key_len = match_len;
+        last_str_hash = matches[m].str_hash;
+    }
+
+    /* walk through the source string and try to find a match */
+    while (j < str_len - shortest_match_len) {
+        /* walk all matches that can fit into the source string at j */
+        for (m = first_valid_m; m < match_cnt; m++) {
+            match_len = matches[m].pair->key_length;
+            /* if a match cannot fit, skip it next time */
+            if (j >= str_len - match_len) {
+                first_valid_m = m + 1;
+                continue;
+            }
+
+            /* if it's the same str_hash as last key */
+            if (matches[m].str_hash == str_hash) {
+                /* use the last computed hash */
+                matches[m].str_hash = last_str_hash;
+            } else {
+                str_hash = matches[m].str_hash;
+
+                /* compute hash of next substring */
+                matches[m].str_hash =
+                        REHASH(str[j], str[j + match_len], str_hash,
+                               matches[m].rem_coef);
+            }
+            match = matches[m].pair->key;
+            match_hash = matches[m].key_hash;
+
+            /* 
+             * compare hashes and memory (if hashes are equal) 
+             */
+            if (match_hash == str_hash &&
+                memcmp(match, str + j, match_len) == 0) {
+                /* 
+                 * match found starting at str[j] (including)
+                 */
+                // TODO: implement
+                // temporary solution for debugging
+                printf("Found match for '%s' at position %d.\n", match, j);
+            }
+
+            /* 
+             * store hash for next match (if it has same length)
+             */
+            last_str_hash = matches[m].str_hash;
+        }
+        j++;
+    }
+}
+
+/** @} */
--- src/str_multireplace.h
+++ src/str_multireplace.h
@@ -1,3 +1,4 @@
+
 /**
  * @file      str_multireplace.h
  * @brief     Header for multiple key-value replacement in provided string.
@@ -18,12 +19,14 @@
 /**
  * Out of memory error
  */
-#define STR_MR_ERROR_OOM	    (-1)
+#define STR_MR_ERROR_OOM            (-1)
+
 /**
  * Invalid argument provided error 
  * (usually means something was NULL where it shouldn't be)
  */
 #define STR_MR_ERROR_INVALID_ARG    (-2)
+
 /**
  * Invalid match pair provided
  * (usually means key or value in some match pair is NULL)
@@ -34,8 +37,10 @@
  * @brief Match key-value string pair
  */
 typedef struct {
-    const char *key;	/**< key that should be replaced */
-    const char *value;	/**< value put in place of key */
+    const char *key;            /**< key that should be replaced */
+    size_t      key_length;     /**< length of the key (w/o NULL termin.) */
+    const char *value;          /**< value put in place of key */
+    size_t      value_length;   /**< length of the value (w/o NULL termin.) */
 } str_mr_match_pair;
 
 /**
@@ -43,14 +48,16 @@ typedef struct {
  *
  * Replaces all occurrences of key from provided match_pairs array with the
  * value associated with that key in specified string.
- * 
+ * Doesn't care about NULL terminators so you can use it on any buffer.
+ *
  * Note: Caller is responsible for freeing the result.
  *
  * @param[in] str source string
+ * @param[in] str_len source string length (without NULL terminator)
  * @param[in] match_pairs match pairs array
  * @param[in] match_pair_cnt number of match pairs in match_pair array
  * @param[out] result newly allocated NULL terminated string containing all 
- *	       replacements
+ *             replacements
  * @param[out] result_len length of result string
  *
  * @return number of replacements made or negative number on error
@@ -59,11 +66,8 @@ typedef struct {
  * @retval STR_MR_ERROR_INVALID_MATCH invalid match pair provided
  */
 int32_t
-str_multireplace(const char *str,
-		 const str_mr_match_pair *match_pairs,
-		 size_t match_pair_cnt,
-		 char **result,
-		 size_t *result_len);
+str_multireplace(const char *str, size_t str_len,
+                 const str_mr_match_pair *match_pairs, size_t match_pair_cnt,
+                 char **result, size_t *result_len);
 
 #endif
-
